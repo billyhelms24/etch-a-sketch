3c4870ba690415fc493bed08ce2a9460948631dc
billyhelms24 <williamtvhelms@gmail.com>
2021-01-20 12:31:48 -0500

    Create 16 divs and append them to main

---
+++ script.js
@@ -0,0 +1,8 @@
+function createGrid() {
+    const main = document.createElement('main');
+
+    for (let i = 0 ; i < 16 ; i++) {
+        const div = document.createElement('div')
+        main.appendChild(div);
+    };
+};
