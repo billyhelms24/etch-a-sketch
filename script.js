2b2fc453b5456abe60e6ed31d63b13f11b66ca5a
billyhelms24 <williamtvhelms@gmail.com>
2021-01-20 12:33:13 -0500

    Append main to body

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function createGrid() {
     const main = document.createElement('main');
+    const body = document.querySelector('body');
 
     for (let i = 0 ; i < 16 ; i++) {
         const div = document.createElement('div')
         main.appendChild(div);
     };
+
+    body.appendChild(main);
 };
