e5230bac76ffc3be19519743ddc8175bc484b7b1
billyhelms24 <williamtvhelms@gmail.com>
2021-01-20 14:28:39 -0500

    Add styling to make grid visible

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function createGrid() {
 
     for (let i = 0 ; i < 16 ; i++) {
         const div = document.createElement('div')
-        div.classList.add("item");
+        div.classList.add("grid-item");
+        div.textContent = i + 1;
         main.appendChild(div);
     };
 
