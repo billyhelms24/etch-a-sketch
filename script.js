de924c7a393b04924d546d6a6a5ba07a7faed98a
billyhelms24 <williamtvhelms@gmail.com>
2021-01-20 12:45:44 -0500

    Add classes to elements created by loop

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ function createGrid() {
 
     for (let i = 0 ; i < 16 ; i++) {
         const div = document.createElement('div')
+        div.classList.add("item");
         main.appendChild(div);
     };
+
+    main.classList.add("grid-container")
 
     body.appendChild(main);
 };
