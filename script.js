9c223b3835a483f3b649020dbe26c240e16ced9e
billyhelms24 <williamtvhelms@gmail.com>
2021-01-21 09:57:48 -0500

    Add removeGrid and resetGrid functions

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-function createGrid() {
-    const grid = document.createElement('div');
-    const body = document.querySelector('body');
+const resetBtn = document.querySelector("button");
+resetBtn.addEventListener("click", resetGrid);
+const grid = document.createElement("div");
+const body = document.querySelector("body");
 
-    for (let i = 0 ; i < 16 ; i++) {
-        const item = document.createElement('div')
-        item.textContent = " "
-        item.classList.add("grid-item");
-        item.addEventListener("mouseover", changeColor)
-        grid.appendChild(item);
-    };
+function createGrid(base) {
+  for (let i = 0; i < base ** 2; i++) {
+    const item = document.createElement("div");
+    item.textContent = " ";
+    item.classList.add("grid-item");
+    item.addEventListener("mouseover", changeColor);
+    grid.appendChild(item);
+  }
 
-    grid.classList.add("grid-container")
+  grid.classList.add("grid-container");
 
-    body.appendChild(grid);
-};
+  body.appendChild(grid);
+}
+
+function removeGrid() {
+  grid.remove();
+}
 
 function changeColor(e) {
-    e.target.classList.add("hovered");
+  e.target.classList.add("hovered");
 }
+
+function resetGrid() {
+  console.log("reset");
+  removeGrid();
+  const userInput = prompt("What size grid? (1-1000)");
+  createGrid(userInput);
+}
+
+createGrid(4);
