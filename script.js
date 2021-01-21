b02fb9fac7982bce84873594d9f3144ff661cb1b
billyhelms24 <williamtvhelms@gmail.com>
2021-01-21 11:06:28 -0500

    Attempt to fix lag

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function removePixels() {
     const items = document.querySelectorAll('.grid-item');
     for (let i = 0 ; i < items.length ; i++) {
         items[i].remove();
-        console.log(items);
     }
 }
 
